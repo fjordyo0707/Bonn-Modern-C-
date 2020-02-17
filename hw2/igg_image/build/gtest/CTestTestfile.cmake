6db1f644db9ce0fb79474cdc00d9d215078f7cd7
fjordyo <fjordyo0707@gmail.com>
2020-02-17 20:45:47 +0800

    remove build

--- hw2/igg_image/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/fjord/Desktop/Modern C++/HW/hw2/igg_image/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- hw2/igg_image/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fjord/Desktop/Modern C++/HW/hw2/igg_image/tests
-# Build directory: /home/fjord/Desktop/Modern C++/HW/hw2/igg_image/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(initial_matrix_test "/home/fjord/Desktop/Modern C++/HW/hw2/igg_image/bin/initial_matrix_test")
-subdirs("../gtest")
