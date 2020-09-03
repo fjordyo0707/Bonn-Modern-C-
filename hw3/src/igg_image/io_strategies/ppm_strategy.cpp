a9d8c60cd584bd7a57aeca34df669e5b509f0926
fjordyo0707 <fjordyo0707@gmail.com>
2020-09-03 21:08:41 +0800

    Finsih hw3

---
+++ hw3/src/igg_image/io_strategies/ppm_strategy.cpp
@@ -0,0 +1,69 @@
+#include "igg_image/io_strategies/ppm_strategy.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+namespace igg {
+
+bool PpmIoStrategy::Write(const std::string& file_name,
+                          const ImageData& data) const {
+    std::fstream file;
+    file.open(file_name, std::ios_base::out);
+    if(!file){
+        std::cerr << "Can't open file!\n";
+        return false; 
+    } else{
+        file << "P3"<< std::endl;
+        file << data.cols << " "<< data.rows << std::endl;
+        file << data.max_val << std::endl;
+        for(int i = 0; i < data.cols * data.rows; i++){
+            file << data.data[0][i] <<" "<< data.data[1][i]<<" "<< data.data[2][i]<<"  ";
+        }
+    }
+    return true;
+}
+
+ImageData PpmIoStrategy::Read(const std::string& file_name) const {
+    std::ifstream file(file_name, std::ios_base::in);;
+    ImageData image_data;
+    std::string line;
+    bool is_read_image_size = false;
+    bool is_read_max_val = false;
+
+    while(!is_read_max_val) {
+        std::getline(file, line);
+
+        if(line[0] == 'P')
+            continue;
+
+        if(line[0] == '#')
+            continue;
+        
+        if(!is_read_image_size){
+            std::stringstream iss(line);
+            iss >> image_data.cols >> image_data.rows;
+            is_read_image_size = true;
+        } else if (!is_read_max_val){
+            std::stringstream iss(line);
+            iss >> image_data.max_val;
+            is_read_max_val = true;
+        }
+    }
+
+    image_data.data.resize(3);
+    for(auto& ch: image_data.data)
+        ch.resize(image_data.cols * image_data.rows);
+    
+    int i = 0;
+    while(!file.eof()){
+        file >> image_data.data[0][i];
+        file >> image_data.data[1][i];
+        file >> image_data.data[2][i];
+        ++i;
+    }
+    file.close();
+    return image_data;
+}
+
+}  // namespace igg
--- hw3/src/main.cpp
+++ hw3/src/main.cpp
@@ -3,12 +3,13 @@
 #include "igg_image/image.h"
 #include "igg_image/io_strategies/dummy_strategy.h"
 #include "igg_image/io_strategies/png_strategy.h"
+#include "igg_image/io_strategies/ppm_strategy.h"
 
 
 int main() {
-    std::string file_name = "../data/lena1.png";
-    std::string down2_name = "../data/lena1_down2.png";
-    igg::PngIoStrategy my_strategy; 
+    std::string file_name = "../data/pbmlib.ascii.ppm";
+    std::string down2_name = "../data/pbmlib_down.ascii.ppm";
+    igg::PpmIoStrategy my_strategy; 
     igg::Image image(my_strategy);
 
     image.ReadFromDisk(file_name);
